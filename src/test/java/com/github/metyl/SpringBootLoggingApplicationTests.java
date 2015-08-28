f90374e69a2c1ef599ee6d01889054bbe8011739
Maciej Matys <maciej.matys@blstream.com>
2015-08-28 16:51:05 +0200

    Initial commit

---
+++ src/test/java/com/github/metyl/SpringBootLoggingApplicationTests.java
@@ -0,0 +1,18 @@
+package com.github.metyl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootLoggingApplication.class)
+@WebAppConfiguration
+public class SpringBootLoggingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
